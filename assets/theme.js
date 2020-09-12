5f14ab34431c5d1120bace13b762b404f94bc993
melih <mlhclskn@gmail.com>
2020-09-13 02:39:33 +0300

    Create theme.js

---
+++ assets/theme.js
@@ -0,0 +1,33 @@
+export const theme = {
+  BRAND_PURPLE: "#59509A",
+  HOME_YELLOW: "#FCF4D0"
+}
+export const lightTheme = {
+  blank: 'white',
+  body: "#F8F8F8",
+  body_100: "#EFEFEF",
+  body_200: "#E2E2E2",
+  body_300: "#C9C9C9",
+  body_400: "#A1A1A1",
+  body_500: "#8A8A8A",
+  body_600: "#797979",
+  body_700: "#464646",
+  body_800: "#2F2F2F",
+  body_900: "#191919",
+  ...theme
+}
+
+export const darkTheme = {
+  blank: 'rgb(0,0,0)',
+  body: "#191919",
+  body_100: "#282828",
+  body_200: "#474747",
+  body_300: "#606060",
+  body_400: "#808080",
+  body_500: "#A0A0A0",
+  body_600: "#868686",
+  body_700: "#C5C5C5",
+  body_800: "#DDDDDD",
+  body_900: "#F8F8F8",
+  ...theme
+}
