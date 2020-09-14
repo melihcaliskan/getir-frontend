217c0ecd3b33d6c3b1c61682a7ef5a77d8854fd1
melih <mlhclskn@gmail.com>
2020-09-14 17:49:40 +0300

    fixed header

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,8 @@
+import { Bag, Dropdown, Heart, Search as SearchIcon } from './icons'
+
+import Fade from 'react-reveal/Fade';
 import Link from 'next/link'
-import { Search as SearchIcon } from './icons'
+import VerticalSeparator from "./VerticalSeparator"
 import styled from 'styled-components'
 
 const Header = styled.header`
@@ -7,11 +10,28 @@ const Header = styled.header`
   justify-content:space-between;
   align-items:center;
   padding:2rem 0;
+
+  .no-mobile{
+    display:inline-flex;
+    align-items:center;
+  }
+  @media only screen and (max-width: 1440px) {
+    .no-mobile{
+      display:none;
+    }
+  }
+
+  @media only screen and (max-width: 1280px) {
+  }
+
+  @media only screen and (max-width: 740px) {
+  }
 `
 
 const Left = styled.div`
   display:flex;
   align-items:center;
+  width:100%;
 `
 
 const Logo = styled.img`
@@ -21,7 +41,8 @@ const Logo = styled.img`
 `
 
 const Search = styled.div`
-  width:410px;
+  width:50%;
+  min-width:240px;
   height:48px;
   border-radius:14px;
   padding:0.8em 1em;
@@ -44,6 +65,11 @@ const Search = styled.div`
   }
 `
 
+const Right = styled.div`
+  display:flex;
+  align-items:center;
+`
+
 const ProfilePicture = styled.img`
   padding:1em;
   width:48px;
@@ -53,24 +79,127 @@ const ProfilePicture = styled.img`
   background-repeat: none;
   background-size:cover;
 `
+
+const Item = styled.div`
+  display:inline-flex;
+  align-items:center;
+  margin-right:1em;
+
+  cursor:pointer;
+  
+  .categories{
+    margin-right:0.3em;
+  }
+
+  .yemek{
+    margin-left:0.2em;
+    margin-bottom:25%;
+    font-weight:bold;
+  }
+
+  .hi{
+    margin-left:2em;
+    margin-right:0.2em;
+    color:#ABABBA;
+  }
+  
+  strong{
+    margin-right:1em;
+  }
+`
+
+const HeartContainer = styled.div`
+  display:inline-flex;
+  justify-content:center;
+  align-items:center;
+  cursor:pointer;
+
+  width:42px;
+  height:42px;
+  border-radius:50%;
+  background:${({ theme }) => theme.HOME_YELLOW};
+`
+
+
+const CartContainer = styled.div`
+  position:relative;
+  display:inline-flex;
+  justify-content:center;
+  align-items:center;
+  cursor:pointer;
+
+  width:42px;
+  height:42px;
+  border-radius:10px;
+  background:${({ theme }) => theme.HOME_APPS_BG};
+  margin-left:1.5em;
+`
+
+const Badge = styled.div`
+  display:inline-flex;
+  justify-content:center;
+  align-items:center;
+  position:absolute;
+  top:-10px;
+  right:-10px;
+  
+  width:24px;
+  height:24px;
+  border-radius:50%;
+
+  font-size:12px;
+  color:${({ theme }) => theme.body};
+  background:${({ theme }) => theme.BRAND_PURPLE};
+  box-shadow: 0px 2px 8px -2px #C1BADE;
+`
+
 const CustomHeader = ({ toggleTheme }) => {
   return (
-    <Header>
-      <Left>
-        <span onClick={() => toggleTheme()}><Logo src="/logo.png" width="80x" alt="getir" /></span>
-        <Search>
-          <input type="text" placeholder="getir'de ara" />
-          <SearchIcon />
-        </Search>
-      </Left>
-      <div>
-        Kategoriler
-      <img src="/logo.png" width="36px" alt="getir yemek" />yemek
+    <Fade duration={700}>
+      <Header>
+        <Left>
+          <span onClick={() => toggleTheme()}><Logo src="/logo.png" width="80x" alt="getir" /></span>
+          <Search>
+            <input type="text" placeholder="getir'de ara" />
+            <SearchIcon />
+          </Search>
+        </Left>
 
-      Selam, <strong>Melih</strong>
-        <ProfilePicture />
-      </div>
-    </Header>
+        <Right>
+          <div className="no-mobile">
+            <Item>
+              <p className="categories">Kategoriler</p>
+              <Dropdown fontSize="14" />
+            </Item>
+
+            <Item>
+              <img src="/logo.png" width="32px" alt="getir yemek" />
+              <p className="yemek">yemek</p>
+            </Item>
+
+            <VerticalSeparator />
+          </div>
+
+          <HeartContainer>
+            <Heart />
+          </HeartContainer>
+
+          <Item>
+            <CartContainer>
+              <Badge>
+                2
+            </Badge>
+              <Bag />
+            </CartContainer>
+          </Item>
+
+          <Item>
+            <p className="hi">Selam,</p><strong>Melih</strong>
+            <ProfilePicture />
+          </Item>
+        </Right>
+      </Header>
+    </Fade>
   )
 }
 
