030d3dd4d8f1d8af5cfd97be19a6518ab541923d
melih <mlhclskn@gmail.com>
2020-09-14 16:43:43 +0300

    created icons

--- components/icons/index.js
+++ components/icons/index.js
@@ -1,6 +1,8 @@
 export { default as Instagram } from './Instagram'
+export { default as AppStore } from './AppStore'
 export { default as Dropdown } from './Dropdown'
 export { default as Facebook } from './Facebook'
+export { default as GooglePlay } from './GooglePlay'
 export { default as Heart } from './Heart'
 export { default as Location } from './Location'
 export { default as Search } from './Search'
