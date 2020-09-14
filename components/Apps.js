dbd6eb67c14493f758ad198517803a70a2f6edc2
melih <mlhclskn@gmail.com>
2020-09-14 21:36:12 +0300

    Update Apps.js

--- components/Apps.js
+++ components/Apps.js
@@ -4,26 +4,39 @@ import Fade from 'react-reveal/Fade';
 import styled from 'styled-components'
 
 const Container = styled.section`
-	display:inline-flex;
-    align-items:center;
-    justify-content:flex-end;
-    position:relative;
+  display:inline-flex;
+  align-items:center;
+  justify-content:flex-end;
+  position:relative;
 
-	margin:15em 0 15em 5em;
-	padding:3em 0 5em 0;
-	border-radius: 40px;
-	background:${({ theme }) => theme.HOME_APPS_BG};
+  margin:15em 0 15em 5em;
+  padding:3em 0 5em 0;
+  border-radius: 40px;
+  background:${({ theme }) => theme.HOME_APPS_BG};
+
+  @media only screen and (max-width: 960px) {
+    margin:8em 0 0 0; 
+    padding:5em 1em 2em 1em;
+    text-align:center;
+  }
 `
 
 const HeroImage = styled.div`
   width:640px;
   height:640px;
   position:absolute;
-  top:-8em;
-  left:-8em;
+  top:0;
+  left:0;
   background-image: url("/phones.png");
   background-size:contain;
   background-repeat:no-repeat;
+
+  @media only screen and (max-width: 960px) {
+    left:25%;
+    top:-5.5em;
+    width:160px;
+    height:160px;
+  }
 `
 
 const HeroText = styled.h2`
@@ -36,7 +49,10 @@ const DetailText = styled.p`
 `
 
 const Links = styled.div`
-	margin-top:2.5em;
+  margin-top:2.5em;
+  @media only screen and (max-width: 960px) {
+    display:flex;
+  }
 `
 
 const Button = styled.a`
@@ -62,33 +78,37 @@ const Button = styled.a`
 
 	&:hover{
     background:${({ theme }) => "#FFFBEB"};
-	}
+  }
+  
+  @media only screen and (max-width: 960px) {
+    font-size:14px;
+  }
 `
 
-const Apps = (props) => {
-	return (
-		<Container>
-			<HeroImage />
-			<div style={{ width: "50%" }}>
-				<HeroText>
-					Getir uygulamasını <br /> simdi indirin.
+const Apps = ({ isMobile }) => {
+  return (
+    <Container>
+      <HeroImage />
+      <div style={{ width: !isMobile && "50%" }}>
+        <HeroText>
+          Getir uygulamasını <br /> simdi indirin.
 					</HeroText>
-				<DetailText>
-					Uygulamayı indirebilmeniz için telefonunuza <br /> bir bağlantı göndereceğiz.
+        <DetailText>
+          Uygulamayı indirebilmeniz için telefonunuza <br /> bir bağlantı göndereceğiz.
 					</DetailText>
 
-				<Links>
-					<Button href="#">
-						<AppStore /> App Store
+        <Links>
+          <Button href="#">
+            <AppStore /> App Store
 						</Button>
 
-					<Button href="#">
-						<GooglePlay /> Google Play
+          <Button href="#">
+            <GooglePlay /> Google Play
 						</Button>
-				</Links>
-			</div>
-		</Container>
-	)
+        </Links>
+      </div>
+    </Container>
+  )
 }
 
 export default Apps
